47ae4e055eb9203bc4b5a406e64fa9293f6e9c8b
endrymateo <endrymateod1011@gmail.com>
2021-03-18 02:42:46 -0400

    setting react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route patch="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
